4e6542bf82b49b64ce7bdbd055d4514bd88570d9
João Viegas <80214969+JoaoantonioViegas@users.noreply.github.com>
2022-05-24 17:00:42 +0100

    profile page in progress

--- standforu/src/components/pages/Profile.js
+++ standforu/src/components/pages/Profile.js
@@ -1,11 +1,33 @@
 import React from "react";
-import Navbar from "../layout/Navbar";
+import Navbar from "../layout/Navbar"
+import'./Profile.css'
+import car1 from '../../images/jeep-wrangler-2006.jpg'
+
 
 function Profile (){
     return (
-        <div>
+        <div className="profilepage">
             <Navbar link="profile"/>
-            <h1>Profile</h1>
+            <div className="profile">
+                <h2 className="dados1">João Viegas</h2>
+                <h2 className="dados2">Joined 20/03/2021</h2>
+                <h2 className="dados3">Jonha@gmail.com</h2>
+                <h2 className="dados4">235649486</h2>
+                <h2 className="dados5">Adoro carros desportivos</h2>
+
+            </div>
+            <div className="mycars">
+                <h1 className="myselcars">My selling cars</h1>
+                    <div className="my1car">
+                        <div className="car1"> 
+                            <img src={car1} alt="car1" className="imageprofcar"/>
+                            <h2 className="letrascarro1">Jeep Rangler</h2>
+                        </div>
+                    </div>
+            </div>
+            <div className="myreviews">
+
+            </div>
         </div>
         
 
