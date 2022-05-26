0f7d03b34e25f256c0bd4d584b6ac73b29fb013d
João Viegas <joao.antonio.viegas@ua.pt>
2022-05-26 16:49:01 +0100

    perfilupgrades

--- standforu/src/components/pages/Profile.js
+++ standforu/src/components/pages/Profile.js
@@ -2,6 +2,8 @@ import React from "react";
 import Navbar from "../layout/Navbar"
 import'./Profile.css'
 import car1 from '../../images/jeep-wrangler-2006.jpg'
+import car2 from '../../images/corolla.jpg'
+import fotoperfil from '../../images/fotoperfil.png'    
 
 
 function Profile (){
@@ -9,11 +11,12 @@ function Profile (){
         <div className="profilepage">
             <Navbar link="profile"/>
             <div className="profile">
+                <img src={fotoperfil} className="fotoperfil"/>
                 <h2 className="dados1">João Viegas</h2>
                 <h2 className="dados2">Joined 20/03/2021</h2>
                 <h2 className="dados3">Jonha@gmail.com</h2>
                 <h2 className="dados4">235649486</h2>
-                <h2 className="dados5">Adoro carros desportivos</h2>
+                <h2 className="dados5">I love sports cars, personally have a Audi A1 and a 1998 nissan GTR</h2>
 
             </div>
             <div className="mycars">
@@ -22,6 +25,12 @@ function Profile (){
                         <div className="car1"> 
                             <img src={car1} alt="car1" className="imageprofcar"/>
                             <h2 className="letrascarro1">Jeep Rangler</h2>
+                        </div>
+                    </div>
+                    <div className="my2car">
+                        <div className="car1"> 
+                            <img src={car2} alt="car1" className="imageprofcar"/>
+                            <h2 className="letrascarro1">Toyota corolla</h2>
                         </div>
                     </div>
             </div>
