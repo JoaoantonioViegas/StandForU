f77ef16537be34400822589876dc67db887ff4c9
João Amaral <jp.amaral@ua.pt>
2022-05-31 15:20:22 +0100

    delete rev

--- standforu/src/components/layout/Embed_reviews.js
+++ standforu/src/components/layout/Embed_reviews.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./Embed_reviews.css";
 import { Icon } from '@iconify/react';
 import { motion } from "framer-motion";
+import Comments from "../../database/comments.json";
 
 function Embed_reviews(props) {
     const {icon,title, description} = props;
 
     const [iconStyle, setIconStyle] = React.useState('');
+    const [revShow, setRevShow] = React.useState(true);
 
     React.useEffect(() => {
         if (icon === 'trash'){
@@ -22,9 +24,9 @@ function Embed_reviews(props) {
 
 
     return (
-        <motion.div animate={{opacity : 1, y:-10}} initial={{opacity:0}} exit={{opacity:0, y:10}} transition={{ duration:0.3}}>
+        <motion.div animate={{opacity : 1, y:-10}} initial={{opacity:0}} exit={{opacity:0, y:10}} transition={{ duration:0.3}} style={{cursor:'pointer', display : revShow? '' : 'none'}}>
             <div className="review-icon">
-                {iconStyle==='trash' && <Icon icon="bi:trash" />}
+                {iconStyle==='trash' && <Icon icon="bi:trash" onClick={() => setRevShow(!revShow)}/>}
                 {iconStyle==='heart' && <Icon  icon="akar-icons:heart" onClick={() => setIconStyle('fav')} style={{cursor:'pointer'}}/>}
                 {iconStyle==='fav' && <Icon  icon="emojione:red-heart" onClick={() => setIconStyle('heart')} style={{cursor:'pointer'}}/>}
             </div> 
