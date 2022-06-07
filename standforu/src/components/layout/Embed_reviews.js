87a63c738e8264e141e273dc6b20f19938f79200
Bruno <blemos@ua.pt>
2022-06-07 16:52:13 +0100

    embed potente e fix problems

--- standforu/src/components/layout/Embed_reviews.js
+++ standforu/src/components/layout/Embed_reviews.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./Embed_reviews.css";
 import { Icon } from '@iconify/react';
 import { motion } from "framer-motion";
@@ -8,7 +9,7 @@ function Embed_reviews(props) {
     console.log(image)
     const [iconStyle, setIconStyle] = React.useState('');
     const [revShow, setRevShow] = React.useState(true);
-
+    const [value,setvalue] = React.useState(randomNumberInRange(10,300));
     React.useEffect(() => {
         if (icon === 'trash'){
             setIconStyle('trash');
@@ -21,6 +22,13 @@ function Embed_reviews(props) {
         }
     }, [icon]);
 
+    
+    const [num, setNum] = useState(0);
+
+    function randomNumberInRange(min, max) {
+      // 👇️ get number between min (inclusive) and max (inclusive)
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     return (
         <motion.div animate={{opacity : 1, y:-10}} initial={{opacity:0}} exit={{opacity:0, y:10}} transition={{ duration:0.3}} style={{cursor:'pointer', display : revShow? '' : 'none'}}>
@@ -36,8 +44,8 @@ function Embed_reviews(props) {
                 </a>
                 <div className="review-icon">
                     {iconStyle==='trash' && <Icon icon="bi:trash" onClick={() => setRevShow(!revShow)}/>}
-                    {iconStyle==='heart' && <Icon  icon="akar-icons:heart" width="25px" onClick={() => setIconStyle('fav')} style={{cursor:'pointer'}}/>}
-                    {iconStyle==='fav' && <Icon  icon="emojione:red-heart" width="25px" onClick={() => setIconStyle('heart')} style={{cursor:'pointer'}}/>}
+                    {iconStyle==='heart' && <div className="divheart"><Icon  icon="akar-icons:heart" width="25px" onClick={() => setIconStyle('fav')} style={{cursor:'pointer'}}/><h4>{value}</h4></div>}
+                    {iconStyle==='fav' && <div className="divheart"><Icon  icon="emojione:red-heart" width="25px" onClick={() => setIconStyle('heart')} style={{cursor:'pointer'}}/><h4>{value}</h4></div>}
                 </div> 
             </div>
         </motion.div>
