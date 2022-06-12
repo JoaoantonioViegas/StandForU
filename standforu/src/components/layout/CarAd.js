b1e17410150d64da94ef90eddb51ab2432a09038
João Viegas <joao.antonio.viegas@ua.pt>
2022-06-12 01:49:35 +0100

    muitas coisas malta

--- standforu/src/components/layout/CarAd.js
+++ standforu/src/components/layout/CarAd.js
@@ -50,9 +50,15 @@ function CarAd({closeAd, visible, opacity, transition, carObject}) {
             <div className='left-container'>
                 <div className='overview'>
                     <img className="carAd-image" src={require('../../images/'+carObject.imagem)}/>
-                    <h1 className='car-name'>{carObject.marca + " " + carObject.modelo}</h1>
+                    <img className="carAd-image" src={require('../../images/'+carObject.imagem2)}/>
                 </div>
                 <div className='car-info'>
+                    
+                </div>
+            </div>
+            <div className='middle-container'>
+                <div className='car-description'>
+                    <h1 className='car-name'>{carObject.marca + " " + carObject.modelo}</h1>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Kms:</em> {carObject.kms} km</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Price:</em> {carObject.preco} €</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Year:</em> {carObject.ano}</p>
@@ -60,25 +66,13 @@ function CarAd({closeAd, visible, opacity, transition, carObject}) {
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Engine:</em> 1995 cm3</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Horsepower:</em> 120cv</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Consumption:</em> 5,9L/100km</p>
-                </div>
-                <button className='reviewsButton' onClick={reviewButton}>Reviews<Icon icon="entypo:popup" className='popupBtn'/></button>
-            </div>
-            <div className='middle-container'>
-                <div className='car-description'>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Advertiser:</em> Professional</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Accepts returns:</em> Yes</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Return value:</em> Same</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Possibility of financing:</em> Yes</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Segment:</em> City</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Transmition:</em> Automatic</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Doors:</em> 5</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Capacity:</em> 5 people</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Vehicle class:</em> Class 1</p>
                     <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Stand warranty:</em> 2 years</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Origin:</em> National</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Non-smoker:</em> Yes</p>
-                    <p><em style={{fontStyle:'normal', fontWeight:'bold'}}>Revision book:</em> Included</p>
                 </div>
+                <button className='reviewsButton' onClick={reviewButton}>Reviews<Icon icon="entypo:popup" className='popupBtn'/></button>
             </div>
             <div className='right-container'>
                 <div className='car-description'>
---
+++ standforu/src/components/layout/Star.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Star(){
+    return (
+        <div className='star'>
+        </div>
+
+    )
+};
+
+export default Star;
